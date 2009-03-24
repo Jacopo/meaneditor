5a38777477f88f2a92cbae6623262af1b519fe13
Jacopo Corbetta <jacopo.corbetta@gmail.com>
2009-03-24 19:46:56 +0100

    i18n for all messages

--- MeanEditor.i18n.php
+++ MeanEditor.i18n.php
@@ -4,5 +4,12 @@ $messages['en'] = array(
 	'prefer_traditonal_editor' => 'Do not use the visual editor',
 	'meaneditor_desc' => 'The mean, the safe and the ugly visual editor for non-technical users. Based on WYMeditor and jQuery.',
 	'no_recent_images' => 'No recently uploaded images.',
-	'try_login' => 'Try logging in first.'
+	'try_login' => 'Try logging in first.',
+	'no_visual' => 'The visual editor can\'t be used for this page. Most likely, it contains advanced or unsopported features. If you can, try editing smaller paragraphs.<br /><br />Reason: ',
+	'reason_whitespace'  => 'Whitespace found at line beginning. Code and preformatted sections are not supported.',
+	'reason_tag' => 'Angle brackets &lt; or &gt; found. HTML and custom tags are not supported (exception: &lt;br /&gt;).',
+	'reason_indent' => 'Found a <b>$1</b> at the beginning of a line. Definition lists and special indents are not supported.',
+	'reason_forbidden_char' => 'Found a <b>$1</b>. Sounds like an unsupported feature.',
+	'reason_special_link' => 'Special wikilinks like <b>$1</b> are not supported.',
+	'checkbox_force_traditional' => 'Force traditional code editing'
 );
