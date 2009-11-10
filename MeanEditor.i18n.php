cc26e7f9127ffda83b0f4bf0db5ac48dffb0286a
wurst <wurst@deskzone.(none)>
2009-11-10 11:27:33 +0100

    added german translation

--- MeanEditor.i18n.php
+++ MeanEditor.i18n.php
@@ -14,3 +14,17 @@ $messages['en'] = array(
 	'checkbox_force_traditional' => 'Force traditional code editing',
 	'recent_images_text' => 'Click <b>here</b> to show images you recently uploaded or type the name of the image. You can also <a href=\'$1\' target=\'_blank\'>upload a new image</a>'
 );
+$messages['de'] = array( 
+	'prefer_traditonal_editor' => 'Den visuellen Editor nicht benutzen.',
+	'meaneditor_desc' => 'Der grafische Editor für nicht technische Nutzer. Basiert auf WYMeditor und jQuery.',
+	'no_recent_images' => 'Keine kürzlich hochgeladenen Bilder.',
+	'try_login' => 'Versuchen sie sich zuerst anzumelden.',
+	'no_visual' => 'Der visuelle Editor kann auf dieser Seite nicht benutzt werden. Wahrscheinlich enthält sie erweiterte oder nicht unterstütze Funktionen. Wenn möglich versuchen sie einzelne Abschnitte zu bearbeiten.<br /><br />Begründung: ',
+	'reason_whitespace'  => 'Whitespace am Zeilenanfang gefunden. Code und preformatted-Abschnitte werden nicht unterstützt.',
+	'reason_tag' => 'Spitze Klammern &lt; oder &gt; gefunden. HTML and benutzerdefinierte tags, mit Ausnahme von &lt;br /&gt;, werden nicht unterstützt.',
+	'reason_indent' => '<b>$1</b> am Zeilenanfang gefunden. Definitionslisten und spezielle Einrückungen werden nicht unterstützt.',
+	'reason_forbidden_char' => '<b>$1</b> gefunden. Vermutlich eine nicht unterstützte Funktion.',
+	'reason_special_link' => 'Spezielle Wikilinks wie <b>$1</b> werden nicht unterstützt.',
+	'checkbox_force_traditional' => 'Traditionelle Bearbeitung erzwingen.',
+	'recent_images_text' => 'Klicke <b>hier</b> um Bilder die kürzlich hochgeladen wurden anzuzeigen oder tippe den Namen eines Bildes. Du kannst auch <a href=\'$1\' target=\'_blank\'>ein neues Bild hochladen.</a>'
+);
